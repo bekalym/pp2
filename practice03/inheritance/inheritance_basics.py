b71a82db9f07e3341f90bf194d80d158229d5eda
bekalym <bekalymeiramkhan@gmail.com>
2026-02-19 02:53:40 +0500

    Complete Practice 3: and inheritance examples

---
+++ practice03/inheritance/inheritance_basics.py
@@ -0,0 +1,73 @@
+#1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("John", "Doe")
+x.printname()
+
+#2
+class Student(Person):
+  def __init__(self, fname, lname, student_id):
+    super().__init__(fname, lname)
+    self.student_id = student_id
+
+  def printname(self):
+    super().printname()
+    print("Student ID:", self.student_id)
+
+y = Student("Jane", "Doe", "S123")
+y.printname()
+
+#3
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+#4
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+#5
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
